053aad04cae212a86beb342819ed479192b8d924
edapess <edmondavetisyanw@gmail.com>
2024-05-20 11:18:18 +0400

    fix: camelcase

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import MainExtractor, { TOptions } from "./MainExtractor";
 
-export const getlinkPreviewData = async (url: string, options?: TOptions) => {
+export const getLinkPreviewData = async (url: string, options?: TOptions) => {
   const extractor = new MainExtractor(url, options);
 
   const previewData = await extractor.getlinkPreviewData();
